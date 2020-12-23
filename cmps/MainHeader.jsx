e9f89091f37b0b0f46777c033bcd798957ebf4a1
Eyal Alfasi <eyalalf@gmail.com>
2020-12-24 00:26:22 +0200

    final commit for the day, added favicon and logo to header

--- cmps/MainHeader.jsx
+++ cmps/MainHeader.jsx
@@ -3,6 +3,10 @@ const { NavLink, withRouter } = ReactRouterDOM;
 function _MainHeader() {
 
     return <header className="main-header-container">
+        <div className="logo-container">
+            <h2>AppSus</h2>
+            <img src="assets\img\logo-white-large.png"/>
+        </div>
         <nav>
             <ul>
                 <li><NavLink activeClassName="nav-active" exact to="/">Home</NavLink></li>
