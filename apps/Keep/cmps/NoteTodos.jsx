57d862315d1fee12aee241e94838fa692446bccc
Daniel Levy <Daniel450Ld@gmail.com>
2020-12-26 19:07:57 +0200

    more keep changes

--- apps/Keep/cmps/NoteTodos.jsx
+++ apps/Keep/cmps/NoteTodos.jsx
@@ -1,15 +1,19 @@
-import {NotesBtn} from './NotesBtn.jsx'
-export function NoteTodos({ note ,onNoteEdit}) {
-    
-    return <section className="note note-todos" style={{backgroundColor: note.style.backgroundColor}}>
+import { NotesBtn } from './NotesBtn.jsx'
+export function NoteTodos({ note, onNoteEdit }) {
+
+    return <section className="note note-todos" style={{ backgroundColor: note.style.backgroundColor }}>
         <h4>{note.info.label}</h4>
-        {note.info.todos.map((todo, idx) => 
-            <section  className="todos-content" key={idx}>
-                <h3>{todo.txt} {todo.doneAt}</h3>
-            </section>
-        )}
-       <NotesBtn  note={note} onNoteEdit={onNoteEdit} />
+        <section>
+            {note.info.todos.map((todo, idx) =>
+                <section className="todos-content" key={idx}>
+                    <h3 onClick={() => onNoteEdit(note, 'Mark')} style={{ textDecorationLine: `${note.isMarked ? 'line-through' : 'none'}` }}>{todo.txt} {todo.doneAt}</h3>
+                </section>
+
+            )}
+        </section>
+
+        <NotesBtn note={note} onNoteEdit={onNoteEdit} />
     </section>
-    
+
 }
- 
+
